8413538b1032729a2abf97710e5efcd2cbe0681c
zgani571-oss <zgani571@gmail.com>
2026-06-28 14:48:39 +0800

    Selesai Bagian 1: Stream dan File

---
+++ Latihan2.java
@@ -0,0 +1,20 @@
+package bagian1.berkas;
+
+import java.io.File;
+import java.io.IOException;
+
+public class Latihan2 {
+    public static void main(String[] args) {
+        File berkas = new File("data.txt");
+
+        try {
+            if (berkas.createNewFile()) {
+                System.out.println("Berkas baru dibuat.");
+            } else {
+                System.out.println("Berkas sudah ada sebelumnya.");
+            }
+        } catch (IOException e) {
+            System.out.println("Terjadi kesalahan: " + e.getMessage());
+        }
+    }
+}
